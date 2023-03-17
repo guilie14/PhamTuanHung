28c659b519761750e50256ee5bbaaab73627e6cc
TuanHungPham <94421733+TuanHungPham@users.noreply.github.com>
2023-03-17 21:13:19 +0700

    Push

---
+++ PhamTuanHung/PhamTuanHung/IStudent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    public interface IStudent
+    {
+        int StudID { get; set; }
+        int StudAge { get; set; }
+        string StudName { get; set; }
+        string StudGender { get; set; }
+        string StudClass { get; set; }
+        float StudAvgMark { get; }
+        void Print();
+    }
+}
---
+++ PhamTuanHung/PhamTuanHung/Student.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    public class Student : IStudent
+    {
+        private int[] MarkList = new int[3];
+
+        public int StudID { get ; set ; }
+        public int StudAge { get ; set ; }
+        public string StudName { get ; set ; }
+        public string StudGender { get ; set; }
+        public string StudClass { get ; set ; }
+
+        public float StudAvgMark { get; private set; }
+
+        public int this[int i]
+        {
+            get { return MarkList[i]; }
+            set { MarkList[i] = value; }
+        }
+
+        public void Print()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Student ID: " + StudID);
+            Console.WriteLine("Student Name: " + StudName);
+            Console.WriteLine("Student Gender: " + StudGender);
+            Console.WriteLine("Student Age: " + StudAge);
+            Console.WriteLine("Student Class: " + StudClass);
+            Console.WriteLine("Student Average Mark: " + StudAvgMark);
+        }
+
+        public void CalAvg()
+        {
+            float sum = 0;
+
+            foreach (int i in MarkList)
+            {
+                sum += i;
+            }
+
+            StudAvgMark = sum / MarkList.Length;
+        }
+    }
+}
